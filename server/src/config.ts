6ac3add3e9215453d58149d72980630393a29b14
lilbonekit <mykytasydorenko0@gmail.com>
2025-03-20 12:24:18 +0200

    Fixed

--- server/src/config.ts
+++ server/src/config.ts
@@ -4,4 +4,5 @@ export const config = {
 	PORT: process.env.PORT,
 	LOG_LEVEL: process.env.LOG_LEVEL || 'info',
 	API_URL: process.env.API_URL,
+	CONTRACT_ADDRESS: process.env.CONTRACT_ADDRESS as string,
 }
--- server/src/utils/claimable-token.ts
+++ server/src/utils/claimable-token.ts
@@ -1,9 +1,10 @@
+import { config } from '../config'
 import { ClaimableToken__factory } from '../types/contracts'
 import { JsonRpcProvider } from 'ethers'
 
 const provider = new JsonRpcProvider('https://l2.rarimo.com')
 const contract = ClaimableToken__factory.connect(
-	'0x69c94172f3E3Cb300e6b0f50A67181455650D150',
+	config.CONTRACT_ADDRESS,
 	provider
 )
 
